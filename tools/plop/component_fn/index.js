163a7140529ea41ddd9d4e50c9c7558620082ebb
Dexter <403634085@qq.com>
2021-03-25 16:12:36 +0800

    加入了一个纯函数的组件create
    Signed-off-by: Dexter <403634085@qq.com>

---
+++ tools/plop/component_fn/index.js
@@ -0,0 +1,47 @@
+const componentExists = require('../utils/componentExists');
+module.exports = {
+  description: '添加一个 component_fn , 为纯函数组件',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: '命名：',
+      default: 'Button',
+      validate: value => {
+        if (/.+/.test(value)) {
+          return componentExists(value)
+            ? '已存在该命名的 Component.'
+            : true;
+        }
+
+        return 'Component Name 不能为空';
+      },
+    },
+  ],
+  actions: data => {
+    // Generate index.js and index.test.js
+
+    const actions = [
+      {
+        type: 'add',
+        path: '../../src/component/{{properCase name}}/index.tsx',
+        templateFile: './component/index.js.hbs',
+        abortOnFail: true,
+      },
+      {
+        type: 'add',
+        path: '../../src/component/{{properCase name}}/index.scss',
+        templateFile: './component/index.scss.hbs',
+        abortOnFail: true,
+      },
+      {
+            type: 'add',
+            path: '../../src/component/{{properCase name}}/index.d.ts',
+            templateFile: './component/index.d.ts.hbs',
+            abortOnFail: true,
+        },
+    ];
+
+    return actions;
+  },
+};
--- tools/plop/index.js
+++ tools/plop/index.js
@@ -1,8 +1,10 @@
 
 let componentGenerator = require('./component/index.js');
+let componentFnGenerator = require('./component_fn/index.js');
 let containerGenerator = require('./container/index.js');
 
 module.exports = function (plop) {
     plop.setGenerator('component', componentGenerator);
+    plop.setGenerator('component_fn', componentFnGenerator);
     plop.setGenerator('container', containerGenerator);
 };
